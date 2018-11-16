c254d63fc801e0a2caa67aafc9543dbf032f82d6
Manuela Epe Arango <1144091759@academico.icesi.edu.co>
2018-11-16 07:44:07 -0500

    try1

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const app = express();
+const fs = require('fs');
+
+app.use(express.static('public'));
+app.engine('handlebars', hbs());
+app.set('view engine','handlebars');
+
+app.listen(5500);
