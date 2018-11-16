613192f92b8d96339abf0c72dfd759ba7f325940
Manuela Epe Arango <1144091759@academico.icesi.edu.co>
2018-11-16 08:08:54 -0500

    Se creo estilos y html de cada página

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ app.use(express.static('public'));
 app.engine('handlebars', hbs());
 app.set('view engine','handlebars');
 
+app.get('/paginaA', function(request, response) {
+    console.log('jucionax1');
+    response.send('juncionax2');
+});
+
+app.get('/', function(request,  response){
+
+    response.send('juncionax3');
+});
 
 app.listen(5500);
